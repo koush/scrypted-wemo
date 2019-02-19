e58d3c542e95d02354f9312ef305f50a627fca38
Koushik Dutta <koushd@gmail.com>
2019-02-18 17:57:55 -0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,23 +1,137 @@
-import axios from 'axios';
+const Wemo = require('wemo-client');
 
-log.i('Hello World');
-log.i('Babel polyfills Promise for us.');
-log.i('setTimeout is implemented by the Android host.');
+Wemo.prototype._listen = function () {
+  // no need to start an http server, use the builtin scrypted one.
+}
 
-function wait(ms) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, ms);
+Wemo.prototype.getCallbackURL = function (opts) {
+  opts = opts || {};
+  if (!this._callbackURL) {
+    var host = this.getLocalInterfaceAddress(opts.clientHostname);
+    this._callbackURL = `http://${host}:9080/endpoint/@scrypted/wemo/public/callback`;
+  }
+  return this._callbackURL;
+}
+
+Wemo.prototype._handleRequest = function (req, res) {
+  var body = req.body;
+  var udn = req.url.replace('/endpoint/@scrypted/wemo/public/callback', '').substring(1);
+
+  if ((req.method == 'NOTIFY') && this._clients[udn]) {
+    // debug('Incoming Request for %s: %s', udn, body);
+    this._clients[udn].handleCallback(body);
+    res.send({
+      code: 204
+    }, null)
+    // res.writeHead(204);
+    // res.end();
+  } else {
+    // debug('Received request for unknown device: %s', udn);
+    res.send({
+      code: 404,
     })
+    // res.writeHead(404);
+    // res.end();
+  }
 }
 
-async function main() {
-    await wait(50);
-    log.i('done waiting for 50ms.');
-    log.i('XMLHttpRequest is polyfilled by the Android host. This allows the popular http library axios or jquery ajax to work.');
+function Device(client, deviceInfo, info) {
+  this.client = client;
+  this.deviceInfo = deviceInfo;
+  this.state = {};
 
-    const ip = await axios.get('http://jsonip.com');
-    log.i(`my ip: ${ip.data.ip}`);
+  this.client.on('binaryState', (state) => this.onEvent('OnOff', null, null, state === '1'));
 }
-main();
 
-log.i('The script will exit, but the async function will continue.');
+Device.prototype.onEvent = function(event, err, response, data) {
+  if (!err) {
+    this.state[event] = data;
+    deviceManager.onDeviceEvent(this.deviceInfo.UDN, event, data)
+  }
+}
+
+Device.prototype.turnOn = function () {
+  this.client.setBinaryState(1, (err, response) => this.onEvent('OnOff', err, response, true));
+}
+Device.prototype.turnOff = function () {
+  this.client.setBinaryState(0, (err, response) => this.onEvent('OnOff', err, response, false));
+}
+Device.prototype.isOn = function () {
+  return this.state.OnOff || false;
+}
+Device.prototype.getLevel = function () {
+  return this.state.Brightness || 0;
+};
+Device.prototype.setLevel = function (level) {
+  this.client.setBrightness(level, (err, response) => this.onEvent('Brightness', err, response, level));
+};
+
+var ServiceTypes = {
+}
+function mapServiceType(serviceType, interfaces, type) {
+  ServiceTypes[serviceType] = {
+    interfaces,
+    type
+  }
+}
+mapServiceType(Wemo.DEVICE_TYPE.Switch, ['OnOff'], 'Outlet');
+mapServiceType(Wemo.DEVICE_TYPE.Dimmer, ['OnOff', 'Brightness'], 'Light');
+mapServiceType(Wemo.DEVICE_TYPE.LightSwitch, ['OnOff'], 'Light');
+
+function DeviceProvider() {
+  var wemo = new Wemo();
+  this.wemo = wemo;
+  this.devices = {};
+
+  wemo.discover((err, deviceInfo) => {
+    console.log(`Wemo Device Found: ${JSON.stringify(deviceInfo)}`);
+
+    // Get the client for the found device
+    var client = wemo.client(deviceInfo);
+
+    // You definitely want to listen to error events (e.g. device went offline),
+    // Node will throw them as an exception if they are left unhandled  
+    client.on('error', (err) => {
+      log.e(`Client Error: ${err.code}`);
+      delete this.devices[deviceInfo.UDN];
+
+      // reconnect?
+    });
+
+    var supportedType = ServiceTypes[deviceInfo.deviceType];
+    if (!supportedType) {
+      return;
+    }
+
+    var interfaces = supportedType.interfaces;
+    var events = interfaces.slice();
+
+    this.devices[deviceInfo.UDN] = new Device(client, deviceInfo);
+    var info = {
+      name: deviceInfo.friendlyName,
+      id: deviceInfo.UDN,
+      interfaces: interfaces,
+      events: events,
+    };
+
+    deviceManager.onDeviceDiscovered(info);
+  });
+}
+
+DeviceProvider.prototype.discoverDevices = function (duration) {
+  log.i('discoverDevices was called!');
+};
+
+DeviceProvider.prototype.getDevice = function (id) {
+  return this.devices[id];
+};
+
+DeviceProvider.prototype.getEndpoint = function () {
+  return '@scrypted/wemo';
+};
+
+DeviceProvider.prototype.onRequest = function (req, res) {
+  this.wemo._handleRequest(req, res)
+};
+
+export default new DeviceProvider();
